1e906c948816e448ece5017218048f946c6fc279
Ohad Levy <ohad1310@gmail.com>
2020-02-21 17:21:08 +0200

    first commit

---
+++ functions.py
@@ -0,0 +1,9 @@
+import mysql connector
+
+
+def sqlconnect:
+    connect = mysql.connector.connect(user='webapp', password='@t8aY2020test', host='<server_address>', database='employee')
+    return
+
+
+def quary:
