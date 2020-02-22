df21c3ff5601656dd855693c5f0ae104c4665329
unknown <ohad1310@gmail.com>
2020-02-22 12:17:05 +0200

    ohad

--- functions.py
+++
@@ -1,9 +0,0 @@
-import mysql connector
-
-
-def sqlconnect:
-    connect = mysql.connector.connect(user='webapp', password='@t8aY2020test', host='<server_address>', database='employee')
-    return
-
-
-def query:
---
+++ var.py
@@ -0,0 +1,7 @@
+
+def mysqlconnect:
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = 'root'
+    app.config['MYSQL_DB'] = 'MyDB'
+    mysql = MySQL(app)
